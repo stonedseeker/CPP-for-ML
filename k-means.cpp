30a4ae3d65e584b95e544672f2cfc7e10c3f55e4
stonedseeker <vaibhav24k@gmail.com>
2025-11-07 23:52:06 +0530

    k-means E

--- k-means.cpp
+++ k-means.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <ctime>
 #include <cmath>
+#include <random>
 #include <limits>
 
 class KMeans {
@@ -11,12 +13,8 @@ private:
 
     std:: vector<std::vector<double>> centroids;
 
-public:
-    KMeans(int numClusters, int maxIter) :
-        k(numClusters), maxIterations(maxIter) {}
-    
-
     double euclideanDistanceSquared(const std::vector<double>& point1, const std::vector<double>& point2) {
+
             double distance = 0.0;
             for (size_t i = 0; i < point1.size(); ++i) {
                 distance += std::pow(point1[i] - point2[i], 2);
@@ -24,11 +22,53 @@ public:
 
             return distance;
     }
+
+
+public:
+    KMeans(int numClusters, int maxIter) :
+        k(numClusters), maxIterations(maxIter) {}
+    
+
     void fit (const std::vector<std::vector<double>>& X) {
-        // TODO 
+        size_t numSamples = X.size();
+        size_t numFeatures = X[0].size();
+
+        centroids.clear()
+
+        std::mt19937 gen(std::time(0));
+        std::uniform_int_distribution<> dis(0, numSamples - 1);
+
+        for (int i = 0; i < k; i++) {
+            int randIndex = dis(gen);
+            centroids.push_back(X[randIndex]);
+            
+            for (size_t j = 0; j < numFeatures; j++) {
+            std::cout << centroids[i][j] << (j == numFeatures - 1 ? "" : ", ");
+            }
+            std::cout << ")" << std::endl;
+        }
     }
 
-    std::vector<int> predict(const std::vector<vector<double>>& X) {
+    for (int iter = 0; iter < maxIterations; ++iter) {
+        std::vector<int> assignments(numSamples, 0);
+
+        for (size_t i = 0; i < numSamples; ++i) {
+            double minDistance = std::numeric_limits<double>::max();
+            int closestCentroidIndex = 0;
+
+            for (int j = 0; j < k; ++j) {
+                double dist = euclideanDistanceSquared(X[i], centroids[j]);
+
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    closestCentroidIndex = j;
+                }
+            }
+            assignments[i] = closestCentroidIndex;
+        }
+    }
+
+    std::vector<int> predict(const std::vector<std::vector<double>>& X) {
         std::vector<int> assignments;
         return assignments;
     }
